296bfddb741ec113f9d4606148647dd470bd7ec0
Allan-Quiterio <allanquiterio100@gmail.com>
2024-03-13 15:58:50 -0300

    refactor: Event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css'],
+  // styleUrls: ['./data-binding.component.css'],
+  // Outra forma de declarar CSS, css inline, recomendado fazer dessa forma quando tem um único estilo dessa forma, passou disso é melhor usar um arquivo à parte
+  styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `,
+  ],
 })
 export class DataBindingComponent implements OnInit {
   url = 'http://loiane.com';
@@ -11,12 +20,33 @@ export class DataBindingComponent implements OnInit {
 
   urlImage = 'https://picsum.photos/200/300.jpg';
 
+  valorAtual = '';
+  valorSalvo = '';
+
+  isMouseOver = false;
+
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valorSalvo) {
+    this.valorSalvo = valorSalvo;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() {}
